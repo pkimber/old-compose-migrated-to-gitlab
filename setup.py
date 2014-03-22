9f37060c47c92fa65d322602d854d65e75025a02
Patrick Kimber <code@pkimber.net>
2014-03-22 11:16:09 +0100

    version 0.0.06

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
             'templates/holding/*.*',
         ],
     },
-    version='0.0.05',
+    version='0.0.06',
     description='holding',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
