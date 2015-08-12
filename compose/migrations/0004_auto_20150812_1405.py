09b4eefa1a273787a5b35c6994e3dbe7e992aeb1
Patrick Kimber <code@pkimber.net>
2015-08-12 14:51:40 +0100

    migrate 'picture' to a 'block.Image' #1011

--- compose/migrations/0004_auto_20150812_1405.py
+++ compose/migrations/0004_auto_20150812_1405.py
@@ -4,12 +4,26 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
-class Migration(migrations.Migration):
+def move_picture(apps, schema_editor):
     """Move contents of 'picture_old' to the 'picture' image link."""
+    article_model = apps.get_model('compose', 'Article')
+    image_model = apps.get_model('block', 'Image')
+    pks = [obj.pk for obj in article_model.objects.all()]
+    for pk in pks:
+        article = article_model.objects.get(pk=pk)
+        # create an 'Image' instance
+        image = image_model(**dict(title='Image Title', image=article.picture_old))
+        image.save()
+        article.picture = image
+        article.save()
+
+
+class Migration(migrations.Migration):
 
     dependencies = [
         ('compose', '0003_auto_20150812_1405'),
     ]
 
     operations = [
+        migrations.RunPython(move_picture),
     ]
