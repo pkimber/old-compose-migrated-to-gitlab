05aa592527e561a1aae0299946a179ad7669dc67
Patrick Kimber <code@pkimber.net>
2015-12-19 22:30:49 +0100

    version 0.0.19

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
             'templates/compose/*.*',
         ],
     },
-    version='0.0.18',
+    version='0.0.19',
     description='compose',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
