2fe9a64ff3e87439e0c96a2766733b670576c17f
Malcolm Dinsmore <zebyea@gmail.com>
2017-01-13 22:47:54 +0100

    version 0.0.28

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
             'templates/compose/*.*',
         ],
     },
-    version='0.0.27',
+    version='0.0.28',
     description='compose',
     author='Malcolm Dinsmore',
     author_email='zebyea@gmail.com',
