10567f7791fbc1319d4bc6c96341cfe02c28809e
Tim Bushell <tcbushell@gmail.com>
2017-03-21 11:32:35 +0200

    More HTML fixes for Navs and Heading

--- bootstrap/views.py
+++ bootstrap/views.py
@@ -2,7 +2,38 @@
 from django.views.generic import TemplateView
 
 from base.view_utils import BaseMixin
+from block.views import CmsPageView
 from compose.models import CodeSnippet
+
+
+class HomeView(CmsPageView):
+
+    template_name = 'web/base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            obj = CodeSnippet.objects.get(slug=CodeSnippet.CSS)
+            snippet = obj.code
+        except CodeSnippet.DoesNotExist:
+            snippet = ''
+        context.update(dict(snippet=snippet))
+        return context
+
+
+class DashView(BaseMixin, TemplateView):
+
+    template_name = 'compose/dash.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            obj = CodeSnippet.objects.get(slug=CodeSnippet.CSS)
+            snippet = obj.code
+        except CodeSnippet.DoesNotExist:
+            snippet = ''
+        context.update(dict(snippet=snippet))
+        return context
 
 
 class DashView(BaseMixin, TemplateView):
