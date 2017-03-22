e0345398348144e2828d39ac6c89757ee81b08b0
Tim Bushell <tcbushell@gmail.com>
2017-03-22 14:26:17 +0200

    Article size settings in example_compose for tests. Tests passing

--- example_compose/base.py
+++ example_compose/base.py
@@ -184,28 +184,25 @@ SENDFILE_BACKEND = 'sendfile.backends.development'
 SENDFILE_ROOT = 'media-private'
 
 
-
-CSS_WIDTH_HALFBIG = 'col-12 col-md-6'
-CSS_WIDTH_FULL = 'col-12'
-CSS_WIDTH_THIRDORSMALLER = 'col-12 col-sm-4 col-md-3'
-CSS_WIDTH_HALFORSMALLER = 'col-12 col-sm-6 col-md-4 col-lg-3'
-CSS_WIDTH_THIRDS = 'col-12 col-sm-6 col-md-4'
+CSS_WIDTH_HALFBIG = 'pure-u-1 pure-md-1-2'
+CSS_WIDTH_FULL = 'pure-u-1'
+CSS_WIDTH_HALFORSMALLER = 'pure-sm-1-2 pure-lg-1-3 pure-xl-1-4'
+CSS_WIDTH_THIRDS = 'pure-u-1-3'
 
 CSS_WIDTHS = (
-    (CSS_WIDTH_HALFBIG, 'Half or Bigger'),
+    (CSS_WIDTH_HALFBIG, 'Half when Big'),
     (CSS_WIDTH_FULL, 'Full Width'),
-    (CSS_WIDTH_THIRDORSMALLER, 'Third or Smaller'),
-    (CSS_WIDTH_HALFORSMALLER, 'Half or Smaller'),
-    (CSS_WIDTH_THIRDS, 'Third or Bigger'),
+    (CSS_WIDTH_HALFORSMALLER, 'Half or Less'),
+    (CSS_WIDTH_THIRDS, 'Text Bottom'),
 )
 
 
-CSS_TEXT_LEFT = 'pull-right ml-3 mb-3 mt-3'
-CSS_TEXT_RIGHT = 'pull-left mr-3 mb-3 mt-3'
-CSS_TEXT_TOP = 'w-100 mb-2 mt-3'
-CSS_TEXT_BOTTOM = 'w-100 mb-3 mt-2'
-CSS_TEXT_ONLY = 'hidden'
-CSS_PICTURE_ONLY = 'visible'
+CSS_TEXT_LEFT = 'text_left'
+CSS_TEXT_RIGHT = 'text_right'
+CSS_TEXT_TOP = 'text_top'
+CSS_TEXT_BOTTOM = 'text_bottom'
+CSS_TEXT_ONLY = 'text_only'
+CSS_PICTURE_ONLY = 'picture_only'
 
 CSS_TEXT_POSITION = (
     (CSS_TEXT_LEFT, 'Text Left'),
@@ -217,17 +214,11 @@ CSS_TEXT_POSITION = (
 )
 
 
-CSS_IMAGE_SIZE_FULL = 'w-100'
-CSS_IMAGE_SIZE_QUARTERS = 'w-75'
-CSS_IMAGE_SIZE_THIRDS = 'w-75'
-CSS_IMAGE_SIZE_HALF = 'w-50'
-CSS_IMAGE_SIZE_THIRD = 'w-25'
-CSS_IMAGE_SIZE_QUARTER = 'w-25'
+CSS_IMAGE_SIZE_HALF = '1-2'
+CSS_IMAGE_SIZE_THIRD = '1-3'
+CSS_IMAGE_SIZE_QUARTER = '1-4'
 
 CSS_IMAGE_SIZES = (
-    (CSS_IMAGE_SIZE_FULL, 'Full Width'),
-    (CSS_IMAGE_SIZE_QUARTERS, '3 Quarters Width'),
-    (CSS_IMAGE_SIZE_THIRDS, '2 Thirds Width'),
     (CSS_IMAGE_SIZE_HALF, 'Half Width'),
     (CSS_IMAGE_SIZE_THIRD, 'Third Width'),
     (CSS_IMAGE_SIZE_QUARTER, 'Quarter Width'),
--- example_compose/dev_test.py
+++ example_compose/dev_test.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from __future__ import unicode_literals
-from .base import *
+from example_compose.base import *
 
 DATABASES = {
     'default': {
@@ -19,4 +19,3 @@ INSTALLED_APPS += (
     'django_extensions',
     'debug_toolbar',
 )
-
